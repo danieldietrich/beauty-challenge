68b4380aa8f7e5ac2f9ab7b56558c03ae1835791
Daniel Dietrich <cafebab3@gmail.com>
2020-10-17 02:08:58 +0200

    Added dark mode button animation

--- src/index.js
+++ src/index.js
@@ -211,14 +211,17 @@ function App() {
       </main>
       <div className={["relative max-w-sm w-full md:w-72 flex flex-col flex-shrink-0 z-40", colors.bgNav].join(' ')}>
         <div class="absolute top-0 right-0 m-1">
-          <button onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')} className="flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600" aria-label="Close sidebar">
-            <svg className={["h-6 w-6", colors.modeButtonText, colors.modeButtonTextHover].join(' ')} stroke="currentColor" fill="none" viewBox="0 0 24 24">
-              {mode === 'dark' ? (
+          <button onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')} className="relative flex items-center justify-center h-12 w-12 rounded-full focus:outline-none focus:bg-gray-600" aria-label="Close sidebar">
+            <div className={["absolute top-0 right-0 transform transition-all ease-in-out duration-500 sm:duration-700", (mode === 'dark' ? 'opacity-100' : 'opacity-0')].join(' ')}>
+              <svg className={["h-6 w-6 transition-opacity ease-in-out duration-500 sm:duration-700", (mode === 'dark' ? 'opacity-100 rotate-0' : 'opacity-0 rotate-180'), colors.modeButtonText, colors.modeButtonTextHover].join(' ')} stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z" />
-              ) : (
+              </svg>
+            </div>
+            <div className={["absolute top-0 right-0 transform transition-all ease-in-out duration-500 sm:duration-700", (mode === 'dark' ? 'opacity-0 rotate-180' : 'opacity-100 rotate-0')].join(' ')}>
+              <svg className={["h-6 w-6", colors.modeButtonText, colors.modeButtonTextHover].join(' ')} stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
-              )}
-            </svg>
+              </svg>
+            </div>
           </button>
         </div>
         <div className="flex-1 h-0 overflow-auto">
