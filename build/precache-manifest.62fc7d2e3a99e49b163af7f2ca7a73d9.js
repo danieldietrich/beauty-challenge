7dcfcc76b31c434c4dfdb39dee4d8cb8e0cccd55
Daniel Dietrich <cafebab3@gmail.com>
2020-10-17 04:27:39 +0200

    release

--- build/precache-manifest.62fc7d2e3a99e49b163af7f2ca7a73d9.js
+++ build/precache-manifest.62fc7d2e3a99e49b163af7f2ca7a73d9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cbc7828240310dd7b70b3f90f95a8002",
+    "revision": "7acde1f68d387ec7b51a60c005da54be",
     "url": "./index.html"
   },
   {
-    "revision": "bdb3bbf167dd07336d83",
+    "revision": "96bd04f3b873da646bf7",
     "url": "./static/css/main.5bfcb158.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.ecdc69f5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bdb3bbf167dd07336d83",
-    "url": "./static/js/main.c60e4abd.chunk.js"
+    "revision": "96bd04f3b873da646bf7",
+    "url": "./static/js/main.0c7b3424.chunk.js"
   },
   {
     "revision": "369d9edb7e26efff363e",
